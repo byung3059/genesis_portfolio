1f7fe9a279aa5bc0aa246346bbe32a6c61479cb4
bys <168626076+byung3059@users.noreply.github.com>
2024-06-18 21:06:56 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,11 @@ $(function () {
 
     const main_visual_slide = new Swiper('.main_visual_slide', {
         loop: true,
+        effect: 'fade',
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
         on: {
             slideChangeTransitionEnd: function () {
                 $('.main_visual .main_visual_list li')
@@ -13,10 +18,31 @@ $(function () {
         }
     });
 
-    $('.main_visual_list li button').on('click', function () {
-        let idx = $(this).parent().index();
+    $('.main_visual_list li').on('click', function () {
+        let idx = $(this).index();
 
         main_visual_slide.slideToLoop(idx, 400);
+    });
+
+    const main_product_slide = new Swiper('.product_slide', {
+
+        slidesPerView: 3,
+        spaceBetween: 30,
+        on: {
+            slideChangeTransitionEnd: function () {
+                $('.main_product_dots li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+            }
+        }
+    })
+
+    $('.main_product_dots li').on('click', function () {
+        let idx = $(this).index();
+
+        main_product_slide.slideTo(idx, 400);
     });
 
 })
