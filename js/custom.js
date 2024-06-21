659e515f754a3ba595558b8a8902519bfdbc1e56
bys <168626076+byung3059@users.noreply.github.com>
2024-06-21 14:44:55 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,51 @@
 $(function () {
 
+    // const el = document.querySelector('.main_product');
+
+
+    // const ani = gsap.timeline({
+
+    //     scrollTrigger: {
+    //         trigger: ".main_product",
+    //         pin: true,
+    //         scrub: 3,
+    //         start: "top top",
+    //         markers: true,
+    //     }
+    // });
+    // const ani2 = gsap.timeline({
+    //     scrollTrigger: {
+    //         trigger: ".main_content",
+    //         pin: true,
+    //         scrub: 5,
+    //         //start: "top top",
+    //         // markers: true,
+    //     }
+    // });
+
+    // ani.from('.main_product .inner', {
+    //     x: 500,
+    // }).to('.main_product .inner', {
+
+    // })
+
+    // ani2.from('.main_content .inner', {
+    //     scale: 1.1,
+    // }).to('.main_content .inner', {
+
+    // })
+
+
+    $('.mbtn').on('click', function () {
+        $(this).toggleClass('on')
+    })
+
+    $('.header .inner .util .mbtn').on('click', function () {
+        $('.header .sub').toggleClass('on')
+    })
+
+
+
     const main_visual_slide = new Swiper('.main_visual_slide', {
         loop: true,
         effect: 'fade',
@@ -45,6 +91,12 @@ $(function () {
         main_product_slide.slideTo(idx, 400);
     });
 
+
+
+
+
+
+
     const main_content_slide = new Swiper('.content_slide', {
         effect: 'fade',
         fadeEffect: {
@@ -65,6 +117,29 @@ $(function () {
         let idx = $(this).index();
 
         main_content_slide.slideTo(idx, 400);
+    })
+
+
+
+    const main_space_slide = new Swiper('.space_slide', {
+
+        slidesPerView: 2,
+        spaceBetween: 30,
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.inner_space .left li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+            }
+        }
+    })
+
+    $('.space_slide_dots li').on('click', function () {
+        let idx = $(this).index();
+
+        main_space_slide.slideToLoop(idx, 400);
     })
 
     AOS.init();
